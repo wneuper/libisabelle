a5fce13851d6e12dc6ef93764192515ef187d499
wneuper <wneuper@ist.tugraz.at>
2015-04-02 15:14:37 +0200

    merged larsh: rework codecs in ML; Scala part broken for now

--- examples/src/main/java/Hello_PIDE.java
+++ examples/src/main/java/Hello_PIDE.java
@@ -6,14 +6,16 @@ import isabelle.XML.Encode.*;    //ERROR
 public class Hello_PIDE {
 
   public static void main(String args[]) {
-    JSystem sys = JSystem.instance(new File("."), "Hello_PIDE");
-    System.out.println(sys.sendCommand("hello", Arrays.asList("world")));
+    JSystem sys = JSystem.instance(new File("."), "Protocol");
+    System.out.println(sys.invoke(Operations.HELLO, "world"));
+//WAS    JSystem sys = JSystem.instance(new File("."), "Hello_PIDE");
+//WAS    System.out.println(sys.sendCommand("hello", Arrays.asList("world")));
     System.out.println("--- test_1:");
     System.out.println(sys.sendCommand("test_1", Arrays.asList(Integer.toString(111))));
     System.out.println("--- test_2:");
     System.out.println(sys.sendCommandXML("test_2", Arrays.asList(isabelle.XML$.Encode$.int_().apply(222) ) ) );
     System.out.println("--- Iterator:");
-//    System.out.println(sys.sendCommandXML("Iterator", Arrays.asList(Integer.toString(1))));
+    //rm ERROR: System.out.println(sys.sendCommandXML("Iterator", Arrays.asList(Integer.toString(1))));
     sys.dispose();
   }
 
