143a82c7719c81ccbd273373f6889457dd9415a9
Walther Neuper <wneuper@ist.tugraz.at>
2015-04-21 16:33:08 +0200

    tuned

--- examples/src/main/java/Hello_PIDE.java
+++ examples/src/main/java/Hello_PIDE.java
@@ -11,7 +11,6 @@ public class Hello_PIDE {
     JSystem sys = JSystem.instance(new File("."), "Protocol");
     System.out.println(sys.invoke(Operations.HELLO, "europa"));
     System.out.println(sys.invoke(Operations.TESTSTR, "xxx"));
-    System.out.println(sys.invoke(Operations.TESTINT, 1));
     sys.dispose();
   }
 
