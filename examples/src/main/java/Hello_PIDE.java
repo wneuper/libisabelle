81fbfbd5cc5635bfb0e699d938f37c6594598fe5
Walther Neuper <wneuper@ist.tugraz.at>
2016-01-15 10:09:57 +0100

    another unsuccessful trial with paths in Hell_PIDE

--- examples/src/main/java/Hello_PIDE.java
+++ examples/src/main/java/Hello_PIDE.java
@@ -6,9 +6,10 @@ import edu.tum.cs.isabelle.setup.*;
 public class Hello_PIDE {
 
   public static void main(String args[]) {
-    Environment env = JSetup.makeEnvironment(Paths.get(System.getenv("/usr/local/isabisac")), 
-      JPlatform.guess(), new Version("2015"));
-    Configuration config = Configuration.fromPath(Paths.get("."), "libisabelle_Isac");
+    Environment env = JSetup.makeEnvironment(
+      Paths.get(System.getenv("/usr/local/isabisac")), JPlatform.guess(), new Version("2015"));
+    Configuration config = Configuration.fromPath(
+      Paths.get("/home/wneuper/proto4/libisabelle-0.2.2"), "libisabelle_Isac");
     JSystem sys = JSystem.create(env, config);
     System.out.println(sys.invoke(Operations.HELLO, "world of libisabelle_Isac"));
     sys.dispose();
